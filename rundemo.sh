428150e81f03a4a91e94ea5c564de400d106e6f2
Greg Gauthier <greg@gmgauthier.com>
2020-10-10 18:00:00 +0100

    collect the pid of the server on startup, and kill it on shutdown

--- rundemo.sh
+++ rundemo.sh
@@ -2,5 +2,6 @@
 
 python3 -m pip install pipenv
 pipenv run ./install.sh
-pipenv run python3 app/simple.py &
+pipenv run python3 app/simple.py & PID=$!
 pipenv run python3 -m pytest tests/test_hov.py --verbose --show-capture=all
+kill -9 $PID
